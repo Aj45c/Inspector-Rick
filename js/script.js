5d756eacf994939d5f275dfcf5913997346f048a
Anthony <anthony.jose53@gmail.com>
2022-05-10 12:33:57 -0400

    Added the modal!! and a button to get the information for the api BUT its not displaying everything at the moment. Working on getting it done, only did rick for now

--- js/script.js
+++ js/script.js
@@ -1,4 +1,39 @@
 //alert("Yo");
 //Characters i used: (717)-Planetina, (329)-Snuffles, (5)-Jerry, (1)-Rick
-const URL = //VARIBLE----------------------------------------------------------
-const URL = "https://rickandmortyapi.com/api/character/?name=Jerry";
+//VARIBLE----------------------------------------------------------
+const URL = "https://rickandmortyapi.com/api/character/1";
+
+// //ELEMENT-----------------------------------------------------------
+const $name = $('#name');
+const $origin = $('#origin');
+const $species = $('#species');
+const $image = $('#img');
+
+//RICKS BUTTON
+const modal = document.querySelector(".popUp");
+const Pop = document.querySelector(".Pop");
+const extBtn = document.querySelector(".exitBtn");
+
+function toggleModal() {
+    modal.classList.toggle("showContent"); //Shows the class that was hidden before
+    
+    $.ajax(URL).then(function(data){
+        $name.text(data.name)
+        $image.image(data.image)
+        $origin.text(data.origin)
+        $species.text(data.species)
+    }, function(error) {
+        console.log('something is wrong')
+        console.log(error)
+    });
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+Pop.addEventListener("click", toggleModal); //Shows the content
+extBtn.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
