2cf6e64119c3ca1f955b98cc21e6b6e943ea22c2
Anthony <anthony.jose53@gmail.com>
2022-05-10 10:11:35 -0400

    Moved everything over to where it should be! Adding Modals for all the buttons that I made

--- js/script.js
+++ js/script.js
@@ -1,2 +1,4 @@
 //alert("Yo");
 //Characters i used: (717)-Planetina, (329)-Snuffles, (5)-Jerry, (1)-Rick
+const URL = //VARIBLE----------------------------------------------------------
+const URL = "https://rickandmortyapi.com/api/character/?name=Jerry";
