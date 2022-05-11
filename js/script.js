c4fb76072b90282430e5cb4b741c7884660e32b1
Anthony <anthony.jose53@gmail.com>
2022-05-11 10:02:38 -0400

    Got all the information to pop up but now i cant do what i want without repeating code. Will finish the other buttons today

--- js/script.js
+++ js/script.js
@@ -1,39 +1,45 @@
 //alert("Yo");
 //Characters i used: (717)-Planetina, (329)-Snuffles, (5)-Jerry, (1)-Rick
 //VARIBLE----------------------------------------------------------
-const URL = "https://rickandmortyapi.com/api/character/1";
+const URL = "";
+const URL3 = "https://rickandmortyapi.com/api/character/717";
+const URL4 = "https://rickandmortyapi.com/api/character/1";
 
 // //ELEMENT-----------------------------------------------------------
 const $name = $('#name');
 const $origin = $('#origin');
 const $species = $('#species');
-const $image = $('#img');
 
-//RICKS BUTTON
-const modal = document.querySelector(".popUp");
-const Pop = document.querySelector(".Pop");
-const extBtn = document.querySelector(".exitBtn");
+//RICKS BUTTON-------------------------------------------------------------
+const modal4 = document.querySelector(".popUp4");
+const Pop4 = document.querySelector(".Pop4");
+const extBtn4 = document.querySelector(".exitBtn4");
+//CONDITION----------------------------------------------------------------
+
 
 function toggleModal() {
-    modal.classList.toggle("showContent"); //Shows the class that was hidden before
+    modal4.classList.toggle("showContent"); //Shows the class that was hidden before
     
-    $.ajax(URL).then(function(data){
+    $.ajax(URL4).then(function(data){
         $name.text(data.name)
-        $image.image(data.image)
-        $origin.text(data.origin)
+        $origin.text(data.origin['name'])
         $species.text(data.species)
+        //$('.popContent').prepend(`<img src = "${data.image}"/>` )
+        console.log(data);
+
     }, function(error) {
         console.log('something is wrong')
         console.log(error)
     });
+
 }
 
 function windowOnClick(event) {
-    if (event.target === modal) {
+    if (event.target === modal4) {
         toggleModal();
     }
 }
 
-Pop.addEventListener("click", toggleModal); //Shows the content
-extBtn.addEventListener("click", toggleModal);
+Pop4.addEventListener("click", toggleModal); //Shows the content
+extBtn4.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
